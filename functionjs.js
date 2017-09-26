000c6b4db145f7b8338db0c0e221f8a82726c34b
Greg <gfisher875@gmail.com>
2017-09-26 12:06:37 -0500

    Function in Javascript.
    Creating a Function File in Javascript.

---
+++ functionjs.js
@@ -0,0 +1,16 @@
+var fruitArray = ["Apple", "Blackberry", "Orange", "Peach", "Strawberry", "Grape". "Raspberry"]
+
+Function getBerries(){
+
+  var results;
+
+  for (var i= 0; i< fruitArray.length; i++){
+
+  if (fruitArray[i].contains("berry")){
+  results += fruitArray[i] + "";
+  }
+  }
+  return results;
+}
+
+var myStringOfBeeries = getBerrie(fruitArray);
--- myJavaScript.js
+++ myJavaScript.js
@@ -1,3 +1,24 @@
+//For Loop
 for (var i = 0; < 5; i++) {
     console.log(i);
 }
+//For/In Loop
+var obj= {a: 1, b: 2, c: 3};
+
+for (var index in obj) {
+  console.log(obj[index]);
+}
+//While loop
+var i = 0;
+
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+//Do/While Loop
+var i = 0;
+
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
